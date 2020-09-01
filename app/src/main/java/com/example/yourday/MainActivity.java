e9f7c1e70b6471a18e24e2869295354769236b5c
Tobias <tobi1879@gmail.com>
2020-09-01 13:55:22 +0200

    Buttons

--- app/src/main/java/com/example/yourday/MainActivity.java
+++ app/src/main/java/com/example/yourday/MainActivity.java
@@ -5,24 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.media.Image;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.example.yourday.model.Day;
+import com.orm.SugarContext;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonUebersicht;
+    Button buttonAuftraege;
     ImageButton buttonKalender;
     DatePickerDialog picker;
     TextView textViewDatum;
+
+    ArrayList<Day> days = new ArrayList<Day>();
+
+    TextView date;
+    TextView ort;
+    SeekBar wieWarTag;
+    TextView erlebnis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SugarContext.init(this);
+
+        buttonAuftraege = (Button) findViewById(R.id.buttonAuftraege);
+        buttonUebersicht = (Button) findViewById(R.id.buttonUebersicht);
         buttonKalender = (ImageButton) findViewById(R.id.imageButtonKalender);
         textViewDatum = (TextView) findViewById(R.id.textViewDatum);
 
@@ -45,6 +67,77 @@ public class MainActivity extends AppCompatActivity {
                 picker.show();
             }
 
+        });
+
+        buttonAuftraege.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Aufträge Klick");
+                setContentView(R.layout.activity_aufgaben);
+            }
+
+        });
+
+        date = (TextView) findViewById(R.id.textViewDatum);
+        ort = (EditText) findViewById(R.id.editTextOrt);
+        wieWarTag = (SeekBar) findViewById(R.id.seekBarWieWarTag);
+        erlebnis = (EditText) findViewById(R.id.editTextTextMultiLineErlebnis);
+
+        ort.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                Boolean alreadyExists = false;
+                int indexOfCurrentDay = 0;
+                for(int i = 0;i<days.size();i++){
+                    if(days.get(i).getDate() == date.getText().toString()){
+                        alreadyExists = true;
+                        indexOfCurrentDay = i;
+                    }
+                }
+                if(alreadyExists == false){
+                    Day day = new Day(date.getText().toString(),ort.getText().toString(),wieWarTag.getProgress(),
+                            erlebnis.getText().toString());
+                    days.add(day);
+                } else {
+                    days.get(indexOfCurrentDay).setOrt(ort.getText().toString());
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
+
+        wieWarTag.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+                Boolean alreadyExists = false;
+                int indexOfCurrentDay = 0;
+                for(int i = 0;i<days.size();i++){
+                    if(days.get(i).getDate() == date.getText().toString()){
+                        alreadyExists = true;
+                        indexOfCurrentDay = i;
+                    }
+                }
+                if(alreadyExists == false){
+                    Day day = new Day(date.getText().toString(),ort.getText().toString(),wieWarTag.getProgress(),
+                            erlebnis.getText().toString());
+                    days.add(day);
+                } else {
+                    days.get(indexOfCurrentDay).setWieWarTag(wieWarTag.getProgress());
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                // TODO Auto-generated method stub
+            }
         });
     }
 }
