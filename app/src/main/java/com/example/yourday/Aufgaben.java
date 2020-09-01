b831851f01ba0c3c44f8487f0e531b94190ce579
turzeno <philipp.rueegg@bwz.rappi.ch>
2020-09-01 13:06:32 +0200

    created "Aufgaben"

--- app/src/main/java/com/example/yourday/Aufgaben.java
+++ app/src/main/java/com/example/yourday/Aufgaben.java
@@ -1,14 +1,71 @@
 package com.example.yourday;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class Aufgaben extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aufgaben);
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
+    }
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Aufgabe entfernt", Toast.LENGTH_LONG).show();
+
+                items.remove(i);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    private void addItem(View view) {
+        EditText input = findViewById(R.id.aufgabenHinzu);
+        String itemText = input.getText().toString();
+
+        if (!(itemText.equals(""))){
+            itemsAdapter.add(itemText);
+            input.setText("");
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Schreibe eine Aufgabe.", Toast.LENGTH_LONG).show();
+        }
     }
 }
