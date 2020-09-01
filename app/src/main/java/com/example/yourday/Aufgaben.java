231a68362f4a8080c3db0ca42f8d096aad168607
turzeno <philipp.rueegg@bwz.rappi.ch>
2020-09-01 10:39:37 +0200

    created screen "Aufgaben"

---
+++ app/src/main/java/com/example/yourday/Aufgaben.java
@@ -0,0 +1,14 @@
+package com.example.yourday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aufgaben extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aufgaben);
+    }
+}
